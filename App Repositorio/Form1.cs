5d73f3235ed0630388242c1c2cebb10222182cc7
Asalmero1 <athos.salmeron@hotmail.com>
2021-05-21 09:39:18 -0500

    actualizacion

--- App Repositorio/Form1.cs
+++ App Repositorio/Form1.cs
@@ -16,5 +16,10 @@ namespace App_Repositorio
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("Hello World");
+        }
     }
 }
